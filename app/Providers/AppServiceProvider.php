fa288836688397f8a44d27b7a1905eba8a799bd0
akmmp241 <akmalmp241@gmail.com>
2023-04-08 06:55:27 +0700

    update AppServiceProvider.php and add register new provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use App\Repositories\Session\SessionRepository;
 use App\Repositories\Session\SessionRepositoryImplement;
 use App\Repositories\User\UserRepository;
 use App\Repositories\User\UserRepositoryImplement;
+use App\Services\Session\SessionService;
+use App\Services\Session\SessionServiceImplement;
 use App\Services\User\UserService;
 use App\Services\User\UserServiceImplement;
 use Illuminate\Support\ServiceProvider;
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->singleton(UserService::class, function ($app) {
             return new UserServiceImplement($app->make(UserRepository::class));
+        });
+
+        $this->app->singleton(SessionService::class, function ($app) {
+            return new SessionServiceImplement($app->make(SessionRepository::class), $app->make(UserRepository::class));
         });
     }
 
