8c89f833b7da7621e367597067857f00b1626ac2
akmmp241 <akmalmp241@gmail.com>
2023-04-08 06:58:59 +0700

    update service layer for session and user

--- app/Services/Session/SessionService.php
+++ app/Services/Session/SessionService.php
@@ -2,9 +2,14 @@
 
 namespace App\Services\Session;
 
+use App\Models\User;
 use LaravelEasyRepository\BaseService;
 
 interface SessionService extends BaseService
 {
+    public function creating(int $id);
 
+    public function destroying(): void;
+
+    public function current(): ?User;
 }
--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -2,9 +2,19 @@
 
 namespace App\Services\User;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\UpdatePasswordRequest;
+use App\Http\Requests\UpdateProfileRequest;
 use LaravelEasyRepository\BaseService;
 
-interface UserService extends BaseService{
+interface UserService extends BaseService
+{
+    public function register(RegisterRequest $request);
 
-    // Write something awesome :)
+    public function login(LoginRequest $request);
+
+    public function updatePassword(UpdatePasswordRequest $request);
+
+    public function updateProfile(UpdateProfileRequest $request);
 }
