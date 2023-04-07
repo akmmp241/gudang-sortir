fb49a64b77ec546e33dcf4368d8832f07f20799f
akmmp241 <akmalmp241@gmail.com>
2023-04-07 13:43:58 +0700

    create service layer then add UserRepository.php and UserRepositoryImplement.php

---
+++ app/Repositories/User/UserRepositoryImplement.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories\User;
+
+use Illuminate\Database\Eloquent\Model;
+use LaravelEasyRepository\Implementations\Eloquent;
+use App\Models\User;
+
+class UserRepositoryImplement extends Eloquent implements UserRepository{
+
+    /**
+    * Model class to be used in this repository for the common methods inside Eloquent
+    * Don't remove or change $this->model variable name
+    * @property Model|mixed $model;
+    */
+    protected User $model;
+
+    public function __construct(User $model)
+    {
+        $this->model = $model;
+    }
+
+    // Write something awesome :)
+}
