89c9f345354fd624a9da1a15ed8f9f01f14ffe53
akmmp241 <akmalmp241@gmail.com>
2023-04-08 06:58:02 +0700

    update repository layer for user and session

--- app/Repositories/Session/SessionRepository.php
+++ app/Repositories/Session/SessionRepository.php
@@ -2,9 +2,15 @@
 
 namespace App\Repositories\Session;
 
+use App\Models\Session;
+use App\Models\User;
 use LaravelEasyRepository\Repository;
 
 interface SessionRepository extends Repository
 {
+    public function save(Session $session);
 
+    public function findByToken(string $token): ?Session;
+
+    public function deleteByToken(string $token);
 }
--- app/Repositories/Session/SessionRepositoryImplement.php
+++ app/Repositories/Session/SessionRepositoryImplement.php
@@ -6,9 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 use LaravelEasyRepository\Implementations\Eloquent;
 use App\Models\Session;
 
-class SessionRepositoryImplement extends Eloquent implements SessionRepository{
+class SessionRepositoryImplement extends Eloquent implements SessionRepository
+{
     public function __construct()
     {
     }
+
+    public function save(Session $session)
+    {
+        $session->save();
+    }
+
+    public function findByToken(string $token): ?Session
+    {
+        return Session::where('token', $token)->first();
+    }
+
+    public function deleteByToken(string $token): void
+    {
+        Session::where('token', $token)->delete();
+    }
+
 
 }
