88043d0123aae56f140b5ed0209d817a69838bef
akmmp241 <akmalmp241@gmail.com>
2023-04-09 18:06:19 +0700

    Create Model Category.php and factory seeder

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id_user' => 1,
+            'category_id' => fake()->userName(),
+            'name_category' => fake()->name(),
+            'description' => fake()->text(100)
+        ];
+    }
+}
